b2004f89d8305f58176eb386d5da3ad28d90190a
mohammad123443214554 <khankadir2703@gmail.com>
2026-03-11 12:35:24 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,22 @@
+import '../styles/globals.css';
+import { AuthProvider } from '../context/AuthContext';
+import { ThemeProvider } from '../context/ThemeContext';
+import Head from 'next/head';
 
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <Head>
+          <title>IDE Game Develop</title>
+          <meta name="description" content="Create games without code. Publish and earn money." />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        </Head>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
