d65aa18af0953bf85f09ba15a4a69770532c974e
mohammad123443214554 <khankadir2703@gmail.com>
2026-03-11 12:34:04 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+// =============================================
+// FILE: postcss.config.js
+// FOLDER: root of project (ide-game-develop/)
+// =============================================
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
