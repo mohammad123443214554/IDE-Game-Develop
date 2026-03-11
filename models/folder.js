fb8d0e23e69a6e6df0ad888fa387e48c8c8cc9f9
mohammad123443214554 <khankadir2703@gmail.com>
2026-03-11 13:37:27 +0530

    Delete models directory

--- models/folder.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const folderSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Folder', folderSchema);
