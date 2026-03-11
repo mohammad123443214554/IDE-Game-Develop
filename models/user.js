35f814325caeff95612ca9dfa6a126185f666557
mohammad123443214554 <khankadir2703@gmail.com>
2026-03-11 12:50:47 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  username: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  bio: { type: String, default: '' },
+  avatar: { type: String, default: '' },
+  isVerified: { type: Boolean, default: false },
+  totalEarnings: { type: Number, default: 0 },
+  availableBalance: { type: Number, default: 0 },
+  totalPlays: { type: Number, default: 0 },
+  otp: { type: String },
+  otpExpires: { type: Date },
+  otpType: { type: String },
+}, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = function(pw) {
+  return bcrypt.compare(pw, this.password);
+};
+
+userSchema.methods.toSafeObject = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.otp;
+  delete obj.otpExpires;
+  return obj;
+};
+
+module.exports = mongoose.model('User', userSchema);
